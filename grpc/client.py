f5d8cb797467ed456d7be35fcd0f87f047e81ccc
mxmfshr <22074734+mxmfshr@users.noreply.github.com>
2020-04-05 01:28:09 +0300

    + grpc tasks 1,2,4

---
+++ grpc/client.py
@@ -0,0 +1,59 @@
+# Copyright 2015 gRPC authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""The Python implementation of the GRPC helloworld.Greeter client."""
+
+from __future__ import print_function
+import logging
+
+import grpc
+
+import myproto_pb2
+import myproto_pb2_grpc
+
+def get_sqrt(stub, val):
+    req = myproto_pb2.SqrtRequest(val=val)
+    response = stub.GetSqrt(req)
+    print(response.res)
+
+def get_sigma(stub, val):
+    reqs = [myproto_pb2.SigmaRequest(val=v) for v in val]
+    response = stub.GetSigma(iter(reqs))
+    print(response.res)
+
+def get_max(stub, val):
+    reqs = [myproto_pb2.MaxRequest(val=v) for v in val]
+    responses = stub.GetMax(iter(reqs))    
+    for res in responses:
+        print(res.res)
+
+def run():
+    channel = grpc.insecure_channel('localhost:50051')
+    stub = myproto_pb2_grpc.MyServiceStub(channel)
+    print('sqrt of 4:')
+    get_sqrt(stub, 4)
+    print()
+
+    print('std of [10,20,30,40,50]:')
+    get_sigma(stub, [10,20,30,40,50])
+    print()
+
+    print('max of [3,4,1,6,2,3,7,10]:')
+    get_max(stub, [3,4,1,6,2,3,7,10])    
+    print()
+    channel.close()
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    run()
---
+++ grpc/server.py
@@ -0,0 +1,51 @@
+# Copyright 2015 gRPC authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""The Python implementation of the GRPC helloworld.Greeter server."""
+
+from concurrent import futures
+import logging
+
+import grpc
+
+import myproto_pb2
+import myproto_pb2_grpc
+
+import math
+import numpy as np
+
+class MyService(myproto_pb2_grpc.MyServiceServicer):
+    def GetSqrt(self, request, context):
+        return myproto_pb2.SqrtReply(res=math.sqrt(request.val))
+
+    def GetSigma(self, request_iterator, context):
+        return myproto_pb2.SigmaReply(res=np.std([v.val for v in request_iterator]))
+
+    def GetMax(self, request_iterator, context):
+        cur_max = None
+        for v in request_iterator:
+            if not cur_max or v.val > cur_max:
+                cur_max = v.val
+                yield myproto_pb2.MaxReply(res=cur_max)
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    myproto_pb2_grpc.add_MyServiceServicer_to_server(MyService(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    serve()
