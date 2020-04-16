310ad129db92e89c304b311d5ced8a798afd60f9
Albina Galimzyanova <albina_kzn98@mail.ru>
2020-04-16 22:58:31 +0300

    ! producer

--- kafka/producer.py
+++ kafka/producer.py
@@ -1,14 +1,10 @@
 from kafka import KafkaProducer
 
-topicName = input('Enter topic name: ')
-text = input('Enter text: ')
 
 bootstrap_servers = ['localhost:9092']
-producer = KafkaProducer(bootstrap_servers = bootstrap_servers)
 producer = KafkaProducer()
 
-ack = producer.send(topicName, text)
-metadata = ack.get()
-
-print(metadata.topic)
-print(metadata.partition)
+while True:
+    topic_name = input('Enter topic name: ')
+    text = input('Enter text: ')
+    producer.send(topic_name, text)
